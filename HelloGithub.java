1963059ff0b77359ab812324c2f2a928e0ed7ff1
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-02 22:24:29 -0300

    final do primeiro projeto

--- HelloGithub.java
+++ HelloGithub.java
@@ -3,11 +3,15 @@ public class HelloGithub {
         String NOME = "Erick Caetano";
         int idade = 23;
         String Cidade = "Brasilia";
-        String msg = "Eu vou ser o melhor Dev do Brasil";
+        String msg = "Eu vou ser o melhor Dev";
         String Frasemarcante = ("Ola eu sou " + NOME + " tenho " + idade + " anos "+ " e moro em " + Cidade + "  " + msg);
-
+        String linguagem = "java";
+        String msge = "Obrigado pela vista ao meu primeiro projeto!!";
 
         System.out.println(Frasemarcante);
+        System.out.println("Aprendendo linguagem em : " + linguagem + " e meu foco e ser um Full Stack da gringa " + msge);
+
+
 
     }
 }
