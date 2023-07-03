b2219b05d49ff2762f8daac568e9484006e15586
Erick Caetano <erick.caetano1200@gmail.com>
2023-07-02 22:10:27 -0300

    Frase marcante

---
+++ HelloGithub.java
@@ -0,0 +1,13 @@
+public class HelloGithub {
+    public static void main(String[] args) {
+        String NOME = "Erick Caetano";
+        int idade = 23;
+        String Cidade = "Brasilia";
+        String msg = "Eu vou ser o melhor Dev do Brasil";
+        String Frasemarcante = ("Ola eu sou " + NOME + " tenho " + idade + " anos "+ " e moro em " + Cidade + "  " + msg);
+
+
+        System.out.println(Frasemarcante);
+
+    }
+}
